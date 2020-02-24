f508ceab2ae658c98d1fc765ae1e42109584d266
sangita avghad <sangita.awaghad@gmail.com>
2020-02-24 11:52:38 -0800

    ADDED:controller for get all books api

---
+++ controllers/bookController.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"awesomeProject1/config"
+	"awesomeProject1/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func GetAllBooks(w http.ResponseWriter,r *http.Request){
+	db,err:=config.GetMySQLDB()
+	if err!=nil{
+		fmt.Println(err)
+
+	}else{
+		bookModel:=models.BookModel{Db:db}
+		books,err:=bookModel.FindAll()
+		if err!=nil{
+			fmt.Println(err)
+		}else{
+			json.NewEncoder(w).Encode(books)
+		}
+	}
+}
+
