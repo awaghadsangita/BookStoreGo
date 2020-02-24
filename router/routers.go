967d83a42bf2438cfe39501e6d5f3b004fdab2fd
sangita avghad <sangita.awaghad@gmail.com>
2020-02-24 15:14:49 -0800

    ADDED:added router for search by title api

--- router/routers.go
+++ router/routers.go
@@ -5,6 +5,7 @@ import(
 	"net/http"
 )
 func HandleRequest(){
+	http.HandleFunc("/getBooksByTitle",controllers.GetBooksByTitle)
 	http.HandleFunc("/getAllBooks",controllers.GetAllBooks)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
